5ffde965cff219d20c6feadfe9fef062578ba7e8
chenxin <chenxin@starlinke.com>
2021-07-23 18:20:08 +0800

    update

--- db/integral.sql
+++ db/integral.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 16/07/2021 18:18:13
+ Date: 23/07/2021 18:19:27
 */
 
 SET NAMES utf8mb4;
@@ -49,7 +49,7 @@ CREATE TABLE `t_grade`  (
   `created_at` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建日期',
   `updated_at` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日期',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_grade
@@ -68,12 +68,19 @@ CREATE TABLE `t_sorce`  (
   `created_at` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '说明',
   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '积分类型：等级、兑换',
-  PRIMARY KEY (`id`) USING BTREE
+  `updated_at` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `status` tinyint NULL DEFAULT NULL COMMENT '状态：有效、无效',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_sorce
 -- ----------------------------
+INSERT INTO `t_sorce` VALUES (1, '1993', 5, '2021-07-16 18:29:23', '签到', '2', NULL, NULL);
+INSERT INTO `t_sorce` VALUES (2, '1993', 12, NULL, '签到', '2', NULL, NULL);
+INSERT INTO `t_sorce` VALUES (3, '1993', 5, '2021-07-23 03:42:52', '做家务', '2', '2021-07-23 03:42:52', NULL);
+INSERT INTO `t_sorce` VALUES (4, '1997', 5, '2021-07-23 06:23:39', '签到', '2', '2021-07-23 06:23:39', NULL);
 
 -- ----------------------------
 -- Table structure for t_user
@@ -81,6 +88,7 @@ CREATE TABLE `t_sorce`  (
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user`  (
   `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '邮箱',
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
   `is_admin` tinyint NULL DEFAULT NULL COMMENT '是否管理员',
@@ -91,13 +99,15 @@ CREATE TABLE `t_user`  (
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
+INSERT INTO `t_user` VALUES ('1993', 'betty_chan@qq.com', '陈欣', '123456', 1, '管理员');
+INSERT INTO `t_user` VALUES ('1997', 'beibei@.qq.com', '贝贝', '123123', 0, NULL);
 
 -- ----------------------------
 -- Table structure for t_wish
 -- ----------------------------
 DROP TABLE IF EXISTS `t_wish`;
 CREATE TABLE `t_wish`  (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '愿望说明',
   `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户',
   PRIMARY KEY (`id`) USING BTREE
@@ -106,5 +116,6 @@ CREATE TABLE `t_wish`  (
 -- ----------------------------
 -- Records of t_wish
 -- ----------------------------
+INSERT INTO `t_wish` VALUES (1, '滑板鞋', '1997');
 
 SET FOREIGN_KEY_CHECKS = 1;
