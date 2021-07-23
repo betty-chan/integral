729824daf4664ff5dae88b60fffca53f930b2bf9
chenxin <chenxin@starlinke.com>
2021-07-23 17:13:15 +0800

    feat :client项目

---
+++ client/pages/tag/tag.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="index">
+		<view class="tag-add">
+			<button type="default">+</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				data: [
+					{
+						id: 1,
+						username: '贝贝',
+						description: 'http://placehold.it/150x150'
+					},
+					{
+						id: 2,
+						username: '兰兰',
+						description: 'http://placehold.it/150x150'
+					},
+				]
+			}
+		},
+		methods: {
+			goList(value) {
+				uni.navigateTo({
+					url:'../list/list?type=' + value.type + '&id=' + value.id
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
